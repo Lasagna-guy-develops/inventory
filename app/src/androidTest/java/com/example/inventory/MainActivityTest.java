204ad20fcfd3ad571750e874749523cd9c8e5898
pinoke98 <jorge_mario98@hotmail.com>
2021-10-23 21:46:26 -0500

    Test Espresso

--- app/src/androidTest/java/com/example/inventory/MainActivityTest.java
+++ app/src/androidTest/java/com/example/inventory/MainActivityTest.java
@@ -1,135 +1,42 @@
 package com.example.inventory;
 
+import static org.junit.Assert.*;
 
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static androidx.test.espresso.action.ViewActions.replaceText;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withParent;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.allOf;
-
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
-
-import androidx.test.espresso.ViewInteraction;
-import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
-import androidx.test.runner.AndroidJUnit4;
 
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
-@LargeTest
-@RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
 
     @Rule
-    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+    public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
 
-    @Test
-    public void mainActivityTest() {
-        ViewInteraction textInputEditText = onView(
-                allOf(withId(R.id.TextInputEditText3),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                3),
-                        isDisplayed()));
-        textInputEditText.perform(replaceText("coca cola"), closeSoftKeyboard());
+    private String Name = "";
+    private int cant = 0;
+    private float price = 0;
 
-        ViewInteraction textInputEditText2 = onView(
-                allOf(withId(R.id.TextInputEditText2),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                2),
-                        isDisplayed()));
-        textInputEditText2.perform(replaceText("200"), closeSoftKeyboard());
-
-        ViewInteraction textInputEditText3 = onView(
-                allOf(withId(R.id.TextInputEditText),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                1),
-                        isDisplayed()));
-        textInputEditText3.perform(replaceText("30"), closeSoftKeyboard());
-
-        ViewInteraction textInputEditText4 = onView(
-                allOf(withId(R.id.TextInputEditText2), withText("200"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                2),
-                        isDisplayed()));
-        textInputEditText4.perform(replaceText("2000"));
-
-        ViewInteraction textInputEditText5 = onView(
-                allOf(withId(R.id.TextInputEditText2), withText("2000"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                2),
-                        isDisplayed()));
-        textInputEditText5.perform(closeSoftKeyboard());
-
-        ViewInteraction materialButton = onView(
-                allOf(withId(R.id.Button), withText("Enviar"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                0),
-                        isDisplayed()));
-        materialButton.perform(click());
-
-        ViewInteraction textView = onView(
-                allOf(withId(R.id.textView), withText("Ingreso Existoso"),
-                        withParent(withParent(withId(android.R.id.content))),
-                        isDisplayed()));
-        textView.check(matches(withText("Ingreso Existoso")));
-
-        ViewInteraction materialButton2 = onView(
-                allOf(withId(R.id.Button), withText("Enviar"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                0),
-                        isDisplayed()));
-        materialButton2.perform(click());
+    @Before
+    public void setUp() throws Exception {
     }
 
-    private static Matcher<View> childAtPosition(
-            final Matcher<View> parentMatcher, final int position) {
+    @Test
+    public void testUserInputScenario() {
+        //Entrar a Ingresar
+        //Ingresar Nombre
+        //Cerrar Keyboard
+        //Ingresar Cantidad
+        //Cerrar Keyboard
+        //Ingresar Precio
+        //Cerrar Keyboard
+        //Undir el Boton
+        //Check Text
+        //Undir el Boton
+    }
 
-        return new TypeSafeMatcher<View>() {
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("Child at position " + position + " in parent ");
-                parentMatcher.describeTo(description);
-            }
-
-            @Override
-            public boolean matchesSafely(View view) {
-                ViewParent parent = view.getParent();
-                return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup) parent).getChildAt(position));
-            }
-        };
+    @After
+    public void tearDown() throws Exception {
     }
 }
